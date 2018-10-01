35137e196ce0aa28dfcbeacf8a53c3a923c22d4a
Michinded <39231587+Michinded@users.noreply.github.com>
2018-09-30 22:25:16 -0500

    Update AreaPentagono.java

--- AreaPentagono.java
+++ AreaPentagono.java
@@ -1,4 +1,20 @@
 
-public class AreaPentagono {
+import java.util.*;
 
+public class AreaPentagono {
+  public static void main(String args[]) {
+		Scanner teclado = new Scanner(System.in);
+		float lado, ap, perimetro, r;
+		
+		System.out.print("Ingrese el valor de un lado: ");
+		lado = teclado.nextFloat();
+		
+		perimetro = lado * 5;
+		
+		ap = (float) (lado / 1.45);
+		
+		r = (perimetro * ap)/2;
+		
+		System.out.print("El area del pentogono es: "+r);
+	}
 }
